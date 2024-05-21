ece4d2e19ffe80e05e657fd59bc986a9d155dfdc
CheslinAbrahams231034 <160215424+CheslinAbrahams231034@users.noreply.github.com>
2024-05-21 13:41:33 +0200

    final Code

--- js/script.js
+++ js/script.js
@@ -1,41 +1,3 @@
-/*
-window.alert = function(message, timeout = null) {
-    const modal = document.getElementById("myModal");
-    const modalMessage = document.getElementById("modal-message");
-    const span = document.getElementsByClassName("close")[0];
-
-
-    modalMessage.innerText = message;
-
-    modal.style.display = "block";
-
-
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-
-    if (timeout != null) {
-        setTimeout(() => {
-            modal.style.display = "none";
-        }, Number(timeout));
-    }
-
-
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
-};
-
-
-function callAlert() {
-    const name = document.getElementById("name").value;
-    const message = `Thank you, ${name}, for submitting your message!`;
-    alert(message, 3000);
-}*/
-
 //This was very mean to me
 
 window.alert = function(message, timeout = null) {
@@ -68,3 +30,17 @@ function callAlert() {
     const message = `Thank you, ${name}, for submitting your message!`;
     alert(message, 3000);
 }
+
+
+
+// Variables for the cart /*this will be for when someone adds an item to the cart and th icon number will increase or decrease.*/
+/*let cartCount = 0;
+
+/*
+if (itemAddedToCart) {
+    cartCount++; // Increase the cart count by 1
+}
+*/
+
+// Display the cart count
+// console.log("Cart Count:", cartCount);
